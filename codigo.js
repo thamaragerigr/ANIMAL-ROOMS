8cb1559dd3f8e510b46b7102be208da8c0340c0b
PauGdM <la.pau.gdm@gmail.com>
2019-11-16 13:30:39 +0100

    resto

--- codigo.js
+++ codigo.js
@@ -1,3 +1,37 @@
+$(document).ready(function() {
+	$('ul li:has(ul)').hover(function(e) {
+		 $(this).find('ul').css({display: "block"});
+	 },
+	 function(e) {
+		 $(this).find('ul').css({display: "none"});
+	 });
+});
+
+var timeout    = 500;
+var closetimer = 0;
+var ddmenuitem = 0;
+
+function jsddm_open()
+{  jsddm_canceltimer();
+   jsddm_close();
+   ddmenuitem = $(this).find('ul').css('visibility', 'visible');}
+
+function jsddm_close()
+{  if(ddmenuitem) ddmenuitem.css('visibility', 'hidden');}
+
+function jsddm_timer()
+{  closetimer = window.setTimeout(jsddm_close, timeout);}
+
+function jsddm_canceltimer()
+{  if(closetimer)
+   {  window.clearTimeout(closetimer);
+      closetimer = null;}}
+
+$(document).ready(function()
+{  $('#jsddm > li').bind('mouseover', jsddm_open)
+   $('#jsddm > li').bind('mouseout',  jsddm_timer)});
+
+document.onclick = jsddm_close;
 $(document).ready(function(){
 	$('ul.tabs li').click(function(){
 		var tab_id = $(this).attr('data-tab');
@@ -30,7 +64,7 @@ function validarCampos(x) {
 			phoneValidation.textContent = 'El número de teléfono no puede contener letras';
 			phoneValidation.classList.remove('d-none');
 		}
-		else if (phone.length === 9) {
+		else if (phone.length >= 9) {
 			phoneValidation.textContent = 'El número de teléfono debe contener 9 dígitos';
 			phoneValidation.classList.remove('d-none');
 		}
@@ -52,3 +86,14 @@ function validarRegexp(campo, regexp) {
 	return todoOK;
 	
 }
+
+
+$(document).ready(function() {
+    $('ul li:has(ul)').hover(function(e) {
+         $(this).find('ul').css({display: "block"});
+     },
+     function(e) {
+         $(this).find('ul').css({display: "none"});
+     });
+});
+
